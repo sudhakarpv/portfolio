617b1ed1ba1adc768e5698401122a0578faef969
sudhakarpv <pvsudhakar1010@gmail.com>
2018-09-26 06:16:27 +0530

    adding jobs

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class blog(models.Model):
     date = models.DateTimeField()
     body = models.TextField()
     imag = models.ImageField(upload_to='image/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return(self.body[:100])
+
+    def date_pretty(self):
+        return(self.date.strftime("%b %e %Y"))
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import blog
 
-# Create your views here.
+
+def allblogs(request):
+    blogs = blog.objects
+    return(render(request, 'blog/blog.html', {'blogs': blogs}))
+
+
+def details(request, blog_id):
+    detailblog = get_object_or_404(blog, pk=blog_id)
+    return (render(request, 'blog/details.html', {'blog': detailblog}))
